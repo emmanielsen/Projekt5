d074ac7c7fd8cad3fa988176431e7ec572f0ba8f
christopherlindgreen <60295770+christopherlindgreen@users.noreply.github.com>
2020-04-08 10:54:56 +0200

    nyalert

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ function newElement() {
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === '') {
-    alert("You must write something!");
+    alert("Husk og tilføj dine vare");
   } else {
     document.getElementById("myUL").appendChild(li);
   }
