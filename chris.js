1741627b43858c64ab972ddf892b8dfc45fc42d9
christopherlindgreen <60295770+christopherlindgreen@users.noreply.github.com>
2020-04-14 22:45:04 +0200

    farvemælk

---
+++ chris.js
@@ -0,0 +1,24 @@
+function on() {
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+  function off() {
+    document.getElementById("overlay").style.display = "none";
+  }
+
+
+
+
+  function getSelectValue()
+  {
+      var selectedValue = document.getElementById("list").value;
+      console.log(selectedValue);
+  }
+  getSelectValue();
+
+  function myFunction() {
+    document.getElementById("mælktekst").style.color = "#7DAEDB";
+  }
+
+
+
