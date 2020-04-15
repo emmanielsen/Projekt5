47666bad76e76d171ae2dd389fd73d2129b1b612
Philsami <58400061+Philsami@users.noreply.github.com>
2020-04-15 10:20:36 +0200

    js fix

--- index.js
+++ index.js
@@ -82,6 +82,35 @@ function myFunction() {
   alert("Du har nu scannet din vare!");
 
 }
+  // Christopher js
+  // Tip
+  
+  
+  function on() {
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+  function off() {
+    document.getElementById("overlay").style.display = "none";
+  }
 
+
+
+  function getSelectValue()
+  {
+      var selectedValue = document.getElementById("list").value;
+      console.log(selectedValue);
+  }
+  getSelectValue();
+
+
+  function myFunction() {
+
+
+      document.getElementById("mælktekst").style.color = "#7DAEDB";
+    
+      document.getElementById("tomattekst").style.color = "#90AE3C";
+      
+    }
 
 
