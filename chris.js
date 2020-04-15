195593aef2084b836f667342f01ac8a389888630
Philsami <58400061+Philsami@users.noreply.github.com>
2020-04-15 11:51:08 +0200

    cfghjk

--- chris.js
+++ chris.js
@@ -1,30 +1,52 @@
+
   // Christopher js
   // Tip
-  
-  
+
+
   function on() {
-      document.getElementById("overlay").style.display = "block";
-    }
-    
-    function off() {
-      document.getElementById("overlay").style.display = "none";
-    }
-  
-  
-  
-    function getSelectValue()
-    {
-        var selectedValue = document.getElementById("list").value;
-        console.log(selectedValue);
-    }
-    getSelectValue();
+    document.getElementById("overlay").style.display = "block";
+  }
+
+  function off() {
+    document.getElementById("overlay").style.display = "none";
+  }
 
 
-    function myFunction() {
+
+  function getSelectValue()
+  {
+      var selectedValue = document.getElementById("list").value;
+      console.log(selectedValue);
+  }
+  getSelectValue();
+
+
+  function myFunction() {
+
+
+      document.getElementById("mælktekst").style.color = "#7DAEDB";
+
+      document.getElementById("tomattekst").style.color = "#90AE3C";
+
+    }
+
+//præmie side med Random Image Generator
   
-  
-        document.getElementById("mælktekst").style.color = "#7DAEDB";
-      
-        document.getElementById("tomattekst").style.color = "#90AE3C";
-        
-      }
+    // Array with random image links is created. You can add as many links as you want/need.
+
+    // Regular Array
+
+    var randomImage = new Array();
+
+    randomImage[0] = "http://url.com/image01.jpg";
+    randomImage[1] = "http://url.com/image02.jpg";
+    randomImage[2] = "http://url.com/image03.jpg";
+    randomImage[3] = "http://url.com/image04.jpg";
+    randomImage[4] = "http://url.com/image05.jpg";
+
+    function getRandomImage() {
+    var number = Math.floor(Math.random()*randomImage.length);
+    document.write('<img src="'+randomImage[number]+'" />');
+    }
+    getRandomImage()
+    //-->
